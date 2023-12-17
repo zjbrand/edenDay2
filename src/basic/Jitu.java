f1e9530dc8c08ec49d7e09533bfb8acaa4c340cb
zjbrand <zjyub1976@gmail.com>
2023-12-17 17:17:34 +0900

    "gaixiu"

--- src/basic/Jitu.java
+++ src/basic/Jitu.java
@@ -9,7 +9,7 @@ public class Jitu {
 		for(int x=0;x<50;x++) {
 			if(2*x+4*(50-x)==120) {
 				
-				System.out.println("农场有鸡："+x+"只");
+				System.out.println("农场有鸡："+x+"");
 				int tu=50-x;
 				System.out.println("农场有兔："+tu+"只");	
 	}
